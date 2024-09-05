7acd7df23cba061595ab813d9e041fa309c1ff9e
almeida1 <engsoftware1@gmail.com>
2024-09-05 08:55:00 -0300

    refatoracao - organizacao dos ct por requitos  Changes to be committed:    modified:   src/REQ01IncremetarTemperatura.test.js 	new file:   src/REQ02DecrementarTemperatura.test.js 	new file:   src/REQ03ResetTemperatrua.test.js

--- src/REQ01IncremetarTemperatura.test.js
+++ src/REQ01IncremetarTemperatura.test.js
@@ -18,3 +18,11 @@ test("CT02- verifica o estado do contador na inicializacao", () => {
   //entao contador = 0
   expect(valorContador).toBe(0);
 });
+test("CT03 - o valor do contador deve incrementar de 1 a cada click do botao", () => {
+  render(<Adiciona />);
+  const btnIcrementa = screen.getByText("Incremento");
+  fireEvent.click(btnIcrementa);
+  fireEvent.click(btnIcrementa);
+  const valorContador = Number(screen.getByTestId("contador").textContent);
+  expect(valorContador).toEqual(2);
+});
